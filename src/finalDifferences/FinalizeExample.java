68e509f9c1a084a3b67863c0cbb0da847a9e5964
Jagruti Goswami <jagrutigoswami402@gmail.com>
2023-08-11 23:25:26 +0530

    finaly and finalize example

--- src/finalDifferences/FinalizeExample.java
+++ src/finalDifferences/FinalizeExample.java
@@ -1,2 +1,31 @@
-package finalDifferences;public class FinalizeExample {
+package finalDifferences;
+import java.util.Scanner;
+
+
+
+import java.util.Scanner;
+
+class ObjectWithFinalize {
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        System.out.println("Finalize method called: Object about to be garbage collected");
+    }
+}
+
+public class FinalizeExample {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ObjectWithFinalize obj = new ObjectWithFinalize();
+        System.out.println("Object created, press Enter to continue...");
+        scanner.nextLine();
+
+        obj = null; // Object becomes eligible for garbage collection
+        System.gc(); // Explicitly request garbage collection
+        System.out.println("Press Enter to exit...");
+        scanner.nextLine();
+
+        scanner.close();
+    }
 }
--- src/finalDifferences/FinallyExample.java
+++ src/finalDifferences/FinallyExample.java
@@ -1,4 +1,4 @@
-package finalKeyword;
+package finalDifferences;
 import java.util.Scanner;
 
 public class FinallyExample {
