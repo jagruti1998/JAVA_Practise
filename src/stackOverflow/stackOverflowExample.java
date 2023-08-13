15e389cde534755ebd0a7c03c3c68470e319ef7d
Jagruti Goswami <jagrutigoswami402@gmail.com>
2023-08-13 13:51:52 +0530

    stack overflow

---
+++ src/stackOverflow/stackOverflowExample.java
@@ -0,0 +1,13 @@
+package stackOverflow;
+
+public class stackOverflowExample {
+    static void recursiveMethod() {
+        System.out.println("Calling recursiveMethod");
+        recursiveMethod(); // Recursive call without base case
+    }
+
+    public static void main(String[] args) {
+        recursiveMethod();
+    }
+}
+  
