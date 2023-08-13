8b5cd4add135d4382db69db82c17e56923688b75
Jagruti Goswami <jagrutigoswami402@gmail.com>
2023-08-13 13:30:08 +0530

    method recursion

---
+++ src/methodRecursion/RecursionExample.java
@@ -0,0 +1,30 @@
+package methodRecursion;
+import java.util.Scanner;
+
+public class RecursionExample {
+
+    // Recursive method to calculate factorial
+    static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a non-negative integer: ");
+        int num = scanner.nextInt();
+
+        if (num < 0) {
+            System.out.println("Invalid input. Please enter a non-negative integer.");
+        } else {
+            int result = factorial(num);
+            System.out.println("Factorial of " + num + " is: " + result);
+        }
+
+        scanner.close();
+    }
+}
